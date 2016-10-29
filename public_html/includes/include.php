8271b9a285916125bdffd16562641b50f29adfdc
th-m <thomvaladez@gmail.com>
2016-10-29 15:59:19 -0600

    abstracting permission :)

--- public_html/includes/include.php
+++ public_html/includes/include.php
@@ -1,6 +1,5 @@
 <?php
-include ('cons.php');
-include ('api_functions.php');
-include ('functions.php');
-
- ?>
+  include ('../../cons.php');
+  include ('api_functions.php');
+  include ('functions.php');
+?>
